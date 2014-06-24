5a9db8d142900fd50bab30f09f13c3f5266ffc9e
bitterg <lovekugua@gmail.com>
2014-06-24 17:55:37 +0800

    add slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,19 @@
 require.config({
 	baseUrl:'assets/js',
 	paths:{
-		jquery:'jquery/jquery-1.7.2.min'
+		jquery:'jquery/jquery-1.7.2.min',
+		'jquery.responsiveslides':'responsiveslides.min'
+	},
+	shim:{
+		'jquery.responsiveslides':['jquery']
 	}
 });
 
-require(['jquery','nav'],function($,nav){
+require(['jquery','nav','jquery.responsiveslides'],function($,nav){
 	$(function(){
 		nav();
+		$(".v-slider").responsiveSlides({
+			pager:true
+		});
 	});
 });
