aaccbc8fd806cfb02880f4904d5b32822035ae95
bitterg <lovekugua@gmail.com>
2014-06-26 09:52:47 +0800

    add product image

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,5 +16,31 @@ require(['jquery','nav','jquery.responsiveslides'],function($,nav){
 		$(".v-slider").responsiveSlides({
 			pager:true
 		});
+		//product page overview/features/specification item exchange func
+		$('.pro-head-rightCon>li').on('click',function(){
+			var $ul = $('.pro-head-rightCon');
+			var $this = $(this);
+			if($this.hasClass('item')&&!$this.hasClass('actived')){
+				$ul.find('li.actived').removeClass('actived');
+				$this.addClass('actived');
+				showContent($this.data('item'));
+			}
+
+			function showContent(item){
+				$(window).scrollTop($('.pro-head').offset().top);
+				switch(item){
+					case "features":
+					$('.vani-conent-intro .section').hide();
+					$('.vani-conent-intro .features').show();
+					break;
+					case "specification":
+					$('.vani-conent-intro .section').hide();
+					$('.vani-conent-intro .specification').show();
+					break;
+					default:
+					$('.vani-conent-intro .section').show();
+				}
+			}
+		});
 	});
 });
